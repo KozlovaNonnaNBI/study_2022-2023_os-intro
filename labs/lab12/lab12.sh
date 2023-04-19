58a291b4e4afb35bf35e5d347671ce9fc6b062d3
KozlovaNonnaNBI <115576317+KozlovaNonnaNBI@users.noreply.github.com>
2023-04-19 10:14:43 +0300

    Add files via upload

---
+++ labs/lab12/lab12.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+lockfile="./lockfile"
+exec {fn}>$lockfile
+echo "lock"
+until flock -n ${fn}
+do
+    echo "not lock"
+    sleep 1
+    flock -n ${fn}
+done
+for ((i=0;i<=5; i++))
+do
+    sleep 1
+done
